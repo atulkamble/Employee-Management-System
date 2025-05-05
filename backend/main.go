f7789c33ce640d92bb5df0f87c19e2665e57fdcd
Atul Kamble <atul_kamble@hotmail.com>
2025-05-05 21:11:32 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
-"os"
-"github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-r := mux.NewRouter()
-r.HandleFunc("/employees", getEmployees).Methods("GET")
-r.HandleFunc("/employees", createEmployee).Methods("POST")
+	r := mux.NewRouter()
+	r.HandleFunc("/employees", getEmployees).Methods("GET")
+	r.HandleFunc("/employees", createEmployee).Methods("POST")
 
-```
-port := "8080"
-fmt.Printf("Server running on port %s\n", port)
-log.Fatal(http.ListenAndServe(":"+port, r))
-```
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
+	fmt.Printf("Server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
