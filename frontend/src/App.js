6e3dcaf76a1bccd08be26ae3c6acf11c86f58be9
Atul Kamble <atul_kamble@hotmail.com>
2025-05-05 21:09:53 +0530

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeList from './components/EmployeeList';
+
+function App() {
+  return (
+    <div>
+      <h1>👩‍💼 Employee Management</h1>
+      <EmployeeList />
+    </div>
+  );
+}
+
+export default App;
